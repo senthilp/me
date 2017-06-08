4b43841f42d106f11b3db5bcd0e4568b4b750d80
senthilp <senthil.padmanabhan@yahoo.com>
2017-06-07 21:46:13 -0700

    Updating SW to production standard

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,60 @@
-const cacheName = 'v1::static';
+const VERSION = 1;
+const ASSETS = [
+    '/',
+    '/images/senthil.png'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll([
-            '/',
-            '/images/senthil.png'
-        ]).then(() => self.skipWaiting(), ex => console.log(`Fetch - ${ex}`)))
-    );
-});
+async function swInstall() {
+    const cache = await caches.open(VERSION);
+    await cache.addAll(ASSETS);
+    await self.skipWaiting();
+}
 
-self.addEventListener('activate', () => self.clients.claim());
+async function swActivate() {
+    const keys = await caches.keys();
+    const deletes = [];
+    for (const key of keys) {
+        if (key !== VERSION) {
+            deletes.push(caches.delete(key));
+        }
+    }
+    await Promise.all(deletes);
+    await self.clients.claim();
+}
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.open(cacheName)
-                            .then(cache => cache.match(event.request)
-                            .then(res => res || fetch(event.request))));
-});
+async function fetchFromNetworkAndCache(req) {
+    const res = await fetch(req);
+    if (!res.url) {
+        // foreign requests will be res.type === 'opaque' and missing a url
+        return res;
+    }
+    const cache = await caches.open(VERSION);
+    cache.put(req, res.clone());
+    return res;
+}
+
+async function fetchFastest(req) {
+    const networkFetch = fetchFromNetworkAndCache(req);
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(req);
+    if (response) {
+        return response;
+    }
+    return networkFetch;
+}
+
+async function swFetch(e) {
+    const req = e.request;
+    const url = new URL(req.url);
+
+    if (req.method !== 'GET' || url.origin !== location.origin) {
+        return;
+    }
+
+    e.respondWith(fetchFastest(req));
+}
+
+self.addEventListener('install', e => e.waitUntil(swInstall()));
+self.addEventListener('activate', e => e.waitUntil(swActivate()));
+self.addEventListener('fetch', e => swFetch(e));
+
